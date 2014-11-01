41bc02381a877aeaa67cb03095966c858eecdf54
Miles Chaston <miles_chaston@yahoo.com>
2014-11-01 09:05:42 -0700

    Created initial AppEngine pieces. Created ant build.

---
+++ src/org/chaston/oakfunds/servlet/WarmupServlet.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Miles Chaston
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.chaston.oakfunds.servlet;
+
+import com.google.inject.Injector;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * TODO(mchaston): write JavaDocs
+ */
+public class WarmupServlet extends HttpServlet {
+
+  private Injector injector;
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    injector = (Injector) getServletContext().getAttribute(Injector.class.getName());
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    // TODO: Warm up the various services.
+  }
+}
