c6d69fe5fc5daf19a89d8d3b366dd64cfcf8b4f5
Miles Chaston <miles_chaston@yahoo.com>
2014-12-06 13:50:12 -0800

    Added models UI placeholder.

--- war/model/controllers.js
+++ war/model/controllers.js
@@ -3,10 +3,19 @@ var modelControllers = angular.module('modelControllers', ['ngRoute']);
 modelControllers.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
+      when('/models', {
+        templateUrl: '/model/models.ng',
+        controller: 'ModelsCtrl'
+      }).
       when('/model_accounts', {
         templateUrl: '/model/accounts.ng',
         controller: 'ModelAccountsCtrl'
       });
+  }]);
+
+modelControllers.controller('ModelsCtrl', ['$scope', '$http', '$window',
+  function ($scope, $http, $window) {
+    // TODO: load models
   }]);
 
 modelControllers.controller('ModelAccountsCtrl', ['$scope', '$http', '$window',
